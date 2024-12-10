b918f78b49627d34474d0f12aca1437434593910
KhanzaidGithub <zaidscestudent@gmail.com>
2024-12-10 02:07:28 -0800

    Start

---
+++ Front-End/src/Components/Protected-Routes/Protected.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom';
+
+
+const Protected = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Protected
